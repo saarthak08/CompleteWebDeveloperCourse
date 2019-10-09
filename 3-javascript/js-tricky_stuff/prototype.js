9af25259379a41b9f29bda5d0dbf02172722f272
Saarthak <saarthakgupta08@gmail.com>
2019-10-10 00:20:43 +0530

    Implemented JavaScript Tricky Stuff

--- 3-javascript/js-tricky_stuff/prototype.js
+++ 3-javascript/js-tricky_stuff/prototype.js
@@ -3,6 +3,7 @@ var Mobile=function(model_no){
     //Instance member               //Classes can also be defined like this.
     this.model_no=model_no;        /*All functions are function objects, classes are functions in turn function objects. Everything is either primitive or an object. */
     this.price=3000;
+    this.a=10;
 };
 class Mobile_Class_Via_Class_Keyword{
     constructor(model){
@@ -50,3 +51,116 @@ function show(){
 }
 
 show();
+
+
+
+//Prototype Inheritance  (Prototype Object is always inherited.)
+Mobile.prototype.z=30;
+//Child Class
+var Xperia=function(){
+    Mobile.call(this);  //In order to make available the parameters of class Mobilw
+    this.b='I am b';
+};
+
+var s=new Xperia();
+console.log(s.a);
+console.log(s.z);
+//Prototype Inheritance
+Xperia.prototype=Object.create(Mobile.prototype);
+Xperia.prototype.constructor=Xperia; //rebuilding constructor
+var k=new Xperia();
+console.log(k.z);
+
+
+
+//Method Overriding
+Mobile.prototype.show=function(){
+    return "Superclass Method";
+}
+
+Xperia.prototype.show=function(){
+    return "ChildClass Method";
+}
+var q=new Mobile("q");
+console.log(k.show());
+console.log(q.show());
+console.log(q);
+console.log(k);
+
+
+
+//Promises in JavaScript
+let promiseToCleanTheRoom=new Promise(function(resolve, reject){
+
+    //cleaning the room
+    let isClean=false;
+    if(isClean){
+        resolve('Clean');
+    }else{
+        reject('not Clean');
+    }
+});
+
+promiseToCleanTheRoom.then(function(fromResolve){
+    console.log(`The room is ` +fromResolve);
+}).catch(function(fromReject){
+    console.log(`The room is `+fromReject);
+});
+
+
+
+
+//Call, Apply & Bind
+var obj={num:2};
+var addToThis=function(a,b,c){
+    return this.num+a+b+c;
+}
+
+console.log(addToThis.call(obj,1,2,3));   //functionname.call(obj,functionarguments);
+
+var arr=[1,3,3];
+console.log(addToThis.apply(obj,arr));
+
+var bound=addToThis.bind(obj);
+console.dir(bound);
+console.log(bound(1,4,5));
+
+
+
+//This in JS
+class abc{
+    constructor(a){
+        this.a=a;
+        abc.prototype.example=()=>{
+            console.log(this.a);
+        }
+    }
+    
+    myFunc(){
+        console.log(this.a);
+    }
+   
+}
+var temp=new abc(20);
+temp.myFunc();
+temp.example();
+
+
+
+
+//Async Await in JavaScript
+function scaryClown() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('🤡');
+      }, 2000);
+    });
+  }
+  
+  async function msg() {
+    const msg = await scaryClown();
+    console.log('Message:', msg);
+  }
+  
+  msg(); // Message: 🤡 <-- after 2 seconds
+  
